0d153deb0c0a98b4bc400d2b41f714d4e2b1e328
Sangmin Lee <gink.ss00@gmail.com>
2024-05-10 10:04:04 +0900

    Feat: 메인페이지 레이아웃 디자인 구현

---
+++ src/app/components/home/layout/styles.ts
@@ -0,0 +1,28 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: url('/assets/hideout.svg');
+  background-size: 100px 100px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 180px;
+  width: 100%;
+  padding: 50px 0;
+  overflow-y: scroll;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
