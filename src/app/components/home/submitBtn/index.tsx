c380a1c8ca9879a982f374f58aa243c82642f42d
Sangmin Lee <gink.ss00@gmail.com>
2024-05-10 09:59:36 +0900

    Feat: 제출 버튼 컴포넌트 및 디자인 구현

---
+++ src/app/components/home/submitBtn/index.tsx
@@ -0,0 +1,20 @@
+import useYearStore from '@/app/stores/year';
+import usePlayerStore from '@/app/stores/player';
+
+import * as S from './styles';
+
+const SubmitBtn = () => {
+  const { selectedYear } = useYearStore();
+  const { selectedTeams } = usePlayerStore();
+
+  return (
+    <S.Button
+      $isActive={!!selectedYear && selectedTeams.length === 5}
+      disabled={!!selectedYear && selectedTeams.length === 5}
+    >
+      CHECK
+    </S.Button>
+  );
+};
+
+export default SubmitBtn;
