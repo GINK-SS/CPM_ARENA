46457aaaa1ec8acd5743e0e3d999b57f0a7458ec
Sangmin Lee <gink.ss00@gmail.com>
2024-05-10 11:11:58 +0900

    Design: 메인페이지 타이틀 디자인 변경

--- src/app/components/home/title/styles.ts
+++ src/app/components/home/title/styles.ts
@@ -16,14 +16,19 @@ export const Wrapper = styled.div`
 `;
 
 export const Title = styled.h1`
-  font-size: 130px;
+  font-size: 150px;
   font-weight: 800;
-  text-shadow: -1px 0px #ff622c, 0px 1px #ff622c, 1px 0px #ff622c, 0px -1px #ff622c;
+  color: transparent;
+  background: linear-gradient(180deg, #eee 20%, #ed5907 100%);
+  background-size: 100% 120%;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
 `;
 
 export const SubTitle = styled.h2`
   padding-bottom: 3px;
   border-bottom: 3px solid #fff;
-  font-size: 40px;
-  font-weight: 600;
+  font-size: 25px;
+  font-weight: 700;
+  opacity: 0.8;
 `;
