619fe323557f74295e03ed29cef74e0a4344adf5
Sangmin Lee <66083363+GINK-SS@users.noreply.github.com>
2024-05-15 00:42:01 +0900

    [chore-22] 메타데이터 보완 및 404페이지 구현 완료 (#23)
    * Feat: 404페이지 구현
    
    * Chore: 메타데이터 타이틀, 설명 및 로봇 추가와 og이미지 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,18 +13,15 @@ const notoSansKR = Noto_Sans_KR({
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://www.cpm-arena.com'),
-  title: '컴투스프로야구for매니저 아레나 도우미 [컴프매 아레나 도우미] | com2usManager Arena Helper',
-  description:
-    '컴투스프로야구for매니저 아레나 도우미 [컴프매 아레나 도우미] | 연도와 팀을 선택 후 오늘의 라인업을 정해 보세요! 오늘의 선수들을 한눈에 확인할 수 있습니다.',
-  keywords:
-    '컴프매, 컴프매 아레나, 컴프매 아레나 도우미, com2us, 컴투스프로야구for매니저, 컴투스프로야구매니저, 컴투스프로야구매니저 아레나 도우미, 아레나, 도우미, com2usmanager, com2usmanagerHelper',
+  title: '컴프매 아레나 도우미 | com2usManager Arena Helper',
+  description: '연도와 팀을 선택 후 나만의 라인업을 구성해 보세요! 선수 정보부터 전력까지 한눈에 확인할 수 있습니다.',
+  keywords: '컴프매, 컴프매 아레나, 컴프매 아레나 도우미, 컴투스프로야구for매니저, 컴투스프로야구매니저 아레나 도우미',
   openGraph: {
     type: 'website',
     url: '/',
-    title: '컴투스프로야구for매니저 아레나 도우미 [컴프매 아레나 도우미] | com2usManager Arena Helper',
-    description:
-      '컴투스프로야구for매니저 아레나 도우미 [컴프매 아레나 도우미] | 연도와 팀을 선택 후 오늘의 라인업을 정해 보세요! 오늘의 선수들을 한눈에 확인할 수 있습니다.',
-    siteName: '컴투스프로야구for매니저 아레나 도우미 [컴프매 아레나 도우미] | com2usManager Arena Helper',
+    title: '컴프매 아레나 도우미 | com2usManager Arena Helper',
+    description: '연도와 팀을 선택 후 나만의 라인업을 구성해 보세요! 선수 정보부터 전력까지 한눈에 확인할 수 있습니다.',
+    siteName: '컴프매 아레나 도우미 - 컴투스프로야구for매니저',
     images: [
       {
         url: '/assets/metaImg.png',
@@ -34,9 +31,8 @@ export const metadata: Metadata = {
   twitter: {
     card: 'summary',
     site: '/',
-    title: '컴투스프로야구for매니저 아레나 도우미 [컴프매 아레나 도우미] | com2usManager Arena Helper',
-    description:
-      '컴투스프로야구for매니저 아레나 도우미 [컴프매 아레나 도우미] | 연도와 팀을 선택 후 오늘의 라인업을 정해 보세요! 오늘의 선수들을 한눈에 확인할 수 있습니다.',
+    title: '컴프매 아레나 도우미 | com2usManager Arena Helper',
+    description: '연도와 팀을 선택 후 나만의 라인업을 구성해 보세요! 선수 정보부터 전력까지 한눈에 확인할 수 있습니다.',
     creator: 'GINK-SS',
     images: '/assets/metaImg.png',
   },
@@ -44,6 +40,9 @@ export const metadata: Metadata = {
     { rel: 'icon', url: '/assets/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
     { rel: 'icon', url: '/assets/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
   ],
+  other: {
+    'naver-site-verification': 'e7d16659e422bbb7d9f376e73462c7c7a162ad49',
+  },
 };
 
 export default function RootLayout({
