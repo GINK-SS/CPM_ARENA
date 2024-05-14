ad4d0bb883f15a6cc9cf2b21f00c1b353eec840b
Sangmin Lee <gink.ss00@gmail.com>
2024-05-14 10:59:47 +0900

    Feat: 메뉴 생성 및 오버롤 필터 기능 구현 및 재설정 버튼 변경
    - 특정 연도 오버롤 문제 때문에 특정 포지션 보이지 않던 문제 해결

--- src/app/stores/table/types.ts
+++ src/app/stores/table/types.ts
@@ -1,5 +1,10 @@
 export type TableStoreState = {
   isShow: boolean;
+  overallLimit: number;
+  isMenu: boolean;
+  setOverallLimit: (selectedOverall: number) => void;
+  openMenu: () => void;
+  closeMenu: () => void;
   showTable: () => void;
   closeTable: () => void;
 };
