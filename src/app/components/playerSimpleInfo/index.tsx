3f6b5f06afc9362d45e74e1ae1cb3d9df2bb765c
GINK-SS <gink.ss00@gmail.com>
2024-06-22 17:46:12 +0900

    Refactor: 선수 간략 정보 스텟 컴포넌트 분리

--- src/app/components/playerSimpleInfo/index.tsx
+++ src/app/components/playerSimpleInfo/index.tsx
@@ -3,8 +3,7 @@ import { IoSearchOutline } from 'react-icons/io5';
 
 import useTeamStore from '@/app/stores/team';
 import usePlayerStore from '@/app/stores/player';
-
-import { isHitter } from '@/app/util/decideType';
+import Stat from './stat';
 
 import * as S from './styles';
 
@@ -47,38 +46,7 @@ const PlayerSimpleInfo = () => {
 
             <S.Overall>{selectedPlayer.overall}</S.Overall>
 
-            <S.StatContainer>
-              <S.StatWrapper>
-                <S.StatName>{isHitter(selectedPlayer) ? '타격' : '변화'}</S.StatName>
-                <S.StatValue $stat={isHitter(selectedPlayer) ? selectedPlayer.batting_all : selectedPlayer.pitch_all}>
-                  {isHitter(selectedPlayer) ? selectedPlayer.batting_all : selectedPlayer.pitch_all}
-                </S.StatValue>
-              </S.StatWrapper>
-              <S.StatWrapper>
-                <S.StatName>{isHitter(selectedPlayer) ? '장타' : '제구'}</S.StatName>
-                <S.StatValue $stat={isHitter(selectedPlayer) ? selectedPlayer.long_all : selectedPlayer.control_all}>
-                  {isHitter(selectedPlayer) ? selectedPlayer.long_all : selectedPlayer.control_all}
-                </S.StatValue>
-              </S.StatWrapper>
-              <S.StatWrapper>
-                <S.StatName>{isHitter(selectedPlayer) ? '선구' : '구위'}</S.StatName>
-                <S.StatValue $stat={isHitter(selectedPlayer) ? selectedPlayer.eye_all : selectedPlayer.stuff_all}>
-                  {isHitter(selectedPlayer) ? selectedPlayer.eye_all : selectedPlayer.stuff_all}
-                </S.StatValue>
-              </S.StatWrapper>
-              <S.StatWrapper>
-                <S.StatName>{isHitter(selectedPlayer) ? '주루' : '멘탈'}</S.StatName>
-                <S.StatValue $stat={isHitter(selectedPlayer) ? selectedPlayer.running : selectedPlayer.mental}>
-                  {isHitter(selectedPlayer) ? selectedPlayer.running : selectedPlayer.mental}
-                </S.StatValue>
-              </S.StatWrapper>
-              <S.StatWrapper>
-                <S.StatName>{isHitter(selectedPlayer) ? '수비' : '체력'}</S.StatName>
-                <S.StatValue $stat={isHitter(selectedPlayer) ? selectedPlayer.defense : selectedPlayer.stamina}>
-                  {isHitter(selectedPlayer) ? selectedPlayer.defense : selectedPlayer.stamina}
-                </S.StatValue>
-              </S.StatWrapper>
-            </S.StatContainer>
+            <Stat />
 
             <S.DetailBtn onClick={onDetailClick}>
               <IoSearchOutline />
