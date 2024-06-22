0e04d3435f5c3550e0f21e0873eb7b98cfd07cb9
GINK-SS <gink.ss00@gmail.com>
2024-06-22 17:57:43 +0900

    Refactor: 총 전력 컴포넌트 분리

---
+++ src/app/components/lineUpInfo/total/index.tsx
@@ -0,0 +1,48 @@
+import useTeamStore from '@/app/stores/team';
+import usePlayerStore from '@/app/stores/player';
+import useBuffStore from '@/app/stores/buff';
+import { BUFF_LIST } from '@/app/const';
+
+import { isTeamBuff } from '@/app/util/decideType';
+import { Team } from '@/app/stores/team/types';
+import { Record } from '@/app/stores/buff/types';
+
+import * as S from './styles';
+
+const Total = () => {
+  const { selectedTeams } = useTeamStore();
+  const { selectedLineup } = usePlayerStore();
+  const { currentBuff } = useBuffStore();
+  const buffOrder: (Team | Record)[] = [...selectedTeams, 'all_star', 'golden_glove', 'mvp'];
+
+  return (
+    <S.Wrapper>
+      <span>총 전력</span>
+      <S.Value>
+        {buffOrder.reduce(
+          (acc, curr) =>
+            acc +
+            (isTeamBuff(curr)
+              ? BUFF_LIST.team.grades.findLastIndex(
+                  (grade) => grade <= currentBuff.teams[selectedTeams.indexOf(curr)]
+                ) === -1
+                ? 0
+                : BUFF_LIST.team.gradeValues[
+                    BUFF_LIST.team.grades.findLastIndex(
+                      (grade) => grade <= currentBuff.teams[selectedTeams.indexOf(curr)]
+                    )
+                  ]
+              : BUFF_LIST[curr].grades.findLastIndex((grade) => grade <= currentBuff[curr]) === -1
+              ? 0
+              : BUFF_LIST[curr]?.gradeValues[
+                  BUFF_LIST[curr]?.grades.findLastIndex((grade) => grade <= currentBuff[curr])
+                ]) *
+              (isTeamBuff(curr) ? currentBuff.teams[selectedTeams.indexOf(curr)] : currentBuff[curr]),
+          0
+        ) + selectedLineup.reduce((acc, curr) => acc + curr.overall, 0)}
+      </S.Value>
+    </S.Wrapper>
+  );
+};
+
+export default Total;
