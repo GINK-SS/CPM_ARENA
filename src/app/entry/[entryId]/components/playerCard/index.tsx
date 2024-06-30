28085b8552cfac40a1fa4f902a78bcf8b1b52856
GINK-SS <gink.ss00@gmail.com>
2024-06-30 21:55:45 +0900

    Feat: 선수 카드 오버롤에 시너지 반영되게 구현

--- src/app/entry/[entryId]/components/playerCard/index.tsx
+++ src/app/entry/[entryId]/components/playerCard/index.tsx
@@ -3,10 +3,12 @@ import { useShallow } from 'zustand/react/shallow';
 
 import useTeamStore from '@/app/stores/team';
 import usePlayerStore from '@/app/stores/player';
-import { TEAMID_TO_SHORTEN } from '@/app/const';
+import useBuffStore from '@/app/stores/buff';
+import { BUFF_LIST, TEAMID_TO_SHORTEN } from '@/app/const';
 
 import { isHitter } from '@/app/util/decideType';
 import { Hitter, HitterPosition, Pitcher, PitcherPosition } from '@/app/stores/player/types';
+import { Records } from '@/app/stores/buff/types';
 
 import * as S from './styles';
 
@@ -18,16 +20,49 @@ type PlayerCardProps = {
 };
 
 const PlayerCard = ({ card: { position, player } }: PlayerCardProps) => {
-  const allTeams = useTeamStore((state) => state.allTeams);
+  const [allTeams, selectedTeams] = useTeamStore(useShallow((state) => [state.allTeams, state.selectedTeams]));
   const [selectedPlayer, pinnedPlayer, setSelectedPlayer, setPinnedPlayer] = usePlayerStore(
     useShallow((state) => [state.selectedPlayer, state.pinnedPlayer, state.setSelectedPlayer, state.setPinnedPlayer])
   );
+  const currentBuff = useBuffStore((state) => state.currentBuff);
   const [pitcherHand, hitterHand] = [player?.hand_type[0], player?.hand_type[2]];
   const imageSrc = !player
     ? ''
     : isHitter(player)
     ? `/assets/player/${TEAMID_TO_SHORTEN[player.team]}_hitter_${hitterHand === '좌' ? 'left' : 'right'}.png`
     : `/assets/player/${TEAMID_TO_SHORTEN[player.team]}_pitcher_${pitcherHand === '좌' ? 'left' : 'right'}.png`;
+
+  const getCalculatedOverall = (player: Hitter | Pitcher) => {
+    let calculatedOverall = player.overall;
+    const records: Records[] = ['all_star', 'golden_glove', 'mvp'];
+    const teamBuffGradesIdx = BUFF_LIST.team.grades.findLastIndex(
+      (grade) => grade <= currentBuff.teams[selectedTeams.findIndex((selectedTeam) => selectedTeam.id === player.team)]
+    );
+
+    calculatedOverall += teamBuffGradesIdx === -1 ? 0 : BUFF_LIST.team.gradeValues[teamBuffGradesIdx];
+    calculatedOverall += records.reduce((acc, curr) => {
+      if (
+        (curr === 'all_star' && !player.all_star) ||
+        (curr === 'golden_glove' && !player.golden_glove) ||
+        (curr === 'mvp' && !(player.mvp_korea || player.mvp_league))
+      ) {
+        return acc;
+      }
+
+      return (
+        acc +
+        (BUFF_LIST[curr].grades.findLastIndex((grade) => grade <= currentBuff[curr]) === -1
+          ? 0
+          : BUFF_LIST[curr].gradeValues[BUFF_LIST[curr].grades.findLastIndex((grade) => grade <= currentBuff[curr])])
+      );
+    }, 0);
+
+    if (!isHitter(player) && player.position !== position) {
+      calculatedOverall -= 3;
+    }
+
+    return calculatedOverall;
+  };
 
   const onClick = () => {
     if (!player) return;
@@ -71,7 +106,7 @@ const PlayerCard = ({ card: { position, player } }: PlayerCardProps) => {
           {player && (
             <>
               <S.Wrapper>
-                <S.Overall>{player.overall - +(!isHitter(player) && player.position !== position) * 3}</S.Overall>
+                <S.Overall>{getCalculatedOverall(player)}</S.Overall>
                 {player.all_star && <Image src={'/assets/all_star.png'} alt='all_star' width={18} height={18} />}
               </S.Wrapper>
 
