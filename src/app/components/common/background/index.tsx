de6b679be5e8df650c145725d0e99abf5ab9121c
GINK-SS <gink.ss00@gmail.com>
2024-07-01 16:29:39 +0900

    Feat: 클릭 효과 구현

--- src/app/components/common/background/index.tsx
+++ src/app/components/common/background/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 
 import * as S from './styles';
 
@@ -7,8 +7,26 @@ type BackgroundProps = {
 };
 
 const Background = ({ children }: BackgroundProps) => {
+  const [clicks, setClicks] = useState<{ x: number; y: number; id: number }[]>([]);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const newClick = {
+      x: event.pageX,
+      y: event.pageY,
+      id: Date.now(),
+    };
+
+    setClicks((prev) => [...prev, newClick]);
+    setTimeout(() => {
+      setClicks((prev) => prev.filter((click) => click.id !== newClick.id));
+    }, 500);
+  };
+
   return (
-    <S.Container>
+    <S.Container onClick={handleClick}>
+      {clicks.map((click) => (
+        <S.ClickEffect key={click.id} style={{ left: click.x, top: click.y }}></S.ClickEffect>
+      ))}
       <S.Content>{children}</S.Content>
     </S.Container>
   );
