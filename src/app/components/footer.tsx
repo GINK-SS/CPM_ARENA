f13c99df730d85d6983416cde81677033687c5e6
GINK-SS <gink.ss00@gmail.com>
2024-07-10 21:17:07 +0900

    Feat: 메인페이지 footer tailwind로 변경

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,24 +1,20 @@
-import * as S from './styles';
-
 const Footer = () => {
   return (
-    <S.Container>
-      <S.Badge>
+    <div className='mx-auto flex max-w-[700px] justify-between gap-3 tablet:flex-col tablet:items-end laptop:max-w-[1000px]'>
+      <div>
         <img
           src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fwww.cpm-arena.com&count_bg=%23D17407&title_bg=%235E5E5E&icon=angellist.svg&icon_color=%23E7E7E7&title=visits&edge_flat=true'
           alt='visits'
-          height={20}
         />
-      </S.Badge>
+      </div>
 
-      <S.Badge>
+      <div>
         <img
-          alt='Static Badge'
           src='https://img.shields.io/badge/contact-gink.ss00%40gmail.com-%23CC7820?style=flat-square&logo=gmail&logoColor=white&labelColor=gray'
-          height={20}
+          alt='contact'
         />
-      </S.Badge>
-    </S.Container>
+      </div>
+    </div>
   );
 };
 
