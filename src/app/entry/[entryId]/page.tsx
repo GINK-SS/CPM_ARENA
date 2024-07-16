7860eca7fe31134f3bbc592249c4583107e6ec54
GINK-SS <gink.ss00@gmail.com>
2024-07-17 00:20:08 +0900

    Refactor: 엔트리 설명 컴포넌트화

--- src/app/entry/[entryId]/page.tsx
+++ src/app/entry/[entryId]/page.tsx
@@ -138,21 +138,7 @@ export default async function Page({ params: { entryId }, searchParams: { limit 
           playersOfSelectedTeams={playersOfSelectedTeams}
           overallLimit={overallLimit}
         />
-
-        <div className='flex w-full justify-around gap-2 border-t-2 border-t-black tablet:gap-3 tablet:border-t-3'>
-          {descriptionList.map((description) => (
-            <div
-              className={classNames(
-                'flex-1 py-9 text-center indent-1 text-10 font-semibold tracking-[1px]',
-                'mobileL:py-13 mobileL:indent-2 mobileL:text-15 mobileL:tracking-[2px] tablet:text-17',
-                'first:bg-[#f0c2bd] last:text-[#1b1588] [&:nth-child(-n+2)]:text-black [&:nth-child(2)]:bg-[#f5df94] [&:nth-child(3)]:text-[#ca4142] [&:nth-last-child(-n+2)]:bg-white'
-              )}
-              key={description}
-            >
-              {description}
-            </div>
-          ))}
-        </div>
+        <EntryDescription />
       </EntryPage>
     </>
   );
