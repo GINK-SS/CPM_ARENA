d7dcd4d7222ca59d0ca424e332c4c8e995b87830
GINK-SS <gink.ss00@gmail.com>
2024-07-18 22:42:44 +0900

    Feat: 메뉴 모달에 추가된 버튼 생성 및 디자인 변경

--- src/app/entry/[entryId]/components/header/menu.tsx
+++ src/app/entry/[entryId]/components/header/menu.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useShallow } from 'zustand/react/shallow';
@@ -10,7 +11,6 @@ import { HiEllipsisVertical } from 'react-icons/hi2';
 import useTableStore from '@/app/stores/table';
 import usePlayerStore from '@/app/stores/player';
 import useBuffStore from '@/app/stores/buff';
-import Image from 'next/image';
 
 export default function Menu() {
   const [isMenu, openMenu, closeMenu] = useTableStore(
@@ -25,7 +25,7 @@ export default function Menu() {
   const searchParams = useSearchParams();
   const buttonValueList = [55, 60, 65, 69];
   const limit = searchParams.get('limit');
-  const overallLimit = !limit || isNaN(+limit) || +limit > 99 || +limit < 55 ? 69 : +limit;
+  const overallLimit = !limit || isNaN(+limit) || +limit > 99 ? 69 : +limit < 55 ? 55 : +limit;
 
   const onFilterClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     setSelectedPlayer(null);
@@ -61,28 +61,71 @@ export default function Menu() {
             </button>
           </div>
 
-          <span className='mb-20 block text-18 font-semibold'>오버롤 설정</span>
+          <div
+            className={classNames('mb-20 border-b-1 border-slate-200/10 pb-20', {
+              hidden: !pathname.startsWith('/entry/'),
+            })}
+          >
+            <span className='mb-20 block text-18 font-semibold'>오버롤 설정</span>
 
-          <ul className='flex flex-wrap items-center justify-center gap-5'>
-            {buttonValueList.map((value) => (
-              <li key={value} className='flex-1'>
-                <button
-                  className={classNames(
-                    'aspect-square w-full rounded-md border-1 text-15 transition-[color,_background-color,_opacity] duration-200',
-                    {
-                      'bg-[#F98A58] font-semibold text-white': value === overallLimit,
-                      'text-slate-400 opacity-50 hover:bg-[#c4a252] hover:text-white hover:opacity-80':
-                        value !== overallLimit,
-                    }
-                  )}
-                  value={value}
-                  onClick={onFilterClick}
-                >
-                  {value === 55 ? '전체' : `${value} 이상`}
-                </button>
-              </li>
-            ))}
-          </ul>
+            <ul className='flex flex-wrap items-center justify-center gap-5'>
+              {buttonValueList.map((value) => (
+                <li key={value} className='flex-1'>
+                  <button
+                    className={classNames(
+                      'aspect-square w-full rounded-md border-1 text-15 transition-[color,_background-color,_opacity] duration-200',
+                      {
+                        'bg-slate-100 font-semibold text-[#e56d36]': value === overallLimit,
+                        'text-slate-400 opacity-50 hover:bg-slate-300 hover:text-slate-800 hover:opacity-90':
+                          value !== overallLimit,
+                      }
+                    )}
+                    value={value}
+                    onClick={onFilterClick}
+                  >
+                    {value === 55 ? '전체' : `${value} 이상`}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className='mb-20 border-b-1 border-slate-200/10 pb-20'>
+            <Link href='/news'>
+              <button
+                className={classNames(
+                  'h-40 w-full rounded-lg border-1 p-8 text-slate-200 shadow-sm duration-200',
+                  'hover:bg-slate-200 hover:text-slate-800'
+                )}
+              >
+                업데이트 내역 / FAQ
+              </button>
+            </Link>
+          </div>
+
+          <Link aria-label='dcinside' href='https://gall.dcinside.com/mgallery/manager' target='_blank'>
+            <button
+              className={classNames(
+                'mb-10 flex h-40 w-full items-center gap-20 rounded-lg border-1 py-8 pl-40 text-slate-200 shadow-sm duration-200',
+                'hover:bg-slate-200 hover:text-slate-800'
+              )}
+            >
+              <Image src={'/assets/dcinside.png'} alt='dc' sizes='50px' width={25} height={25} />
+              디시 컴프매 갤러리
+            </button>
+          </Link>
+
+          <Link aria-label='naver-cafe' href='https://cafe.naver.com/com2usmanager' target='_blank'>
+            <button
+              className={classNames(
+                'flex h-40 w-full items-center gap-20 rounded-lg border-1 py-8 pl-40 text-slate-200 shadow-sm duration-200',
+                'hover:bg-slate-200 hover:text-slate-800'
+              )}
+            >
+              <Image src={'/assets/naver_cafe.webp'} alt='cafe' sizes='50px' width={25} height={25} />
+              네이버 공식카페
+            </button>
+          </Link>
 
           <div className='mt-20 flex items-center justify-between border-t border-slate-200/10 pt-20'>
             <span className='font-semibold'>Contact.</span>
