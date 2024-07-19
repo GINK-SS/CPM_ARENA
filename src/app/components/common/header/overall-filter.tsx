f8bb91be98ccb0f238a089d0aaa37a3151bdfcdb
GINK-SS <gink.ss00@gmail.com>
2024-07-19 23:29:29 +0900

    Feat: 엔트리 페이지일 때, 아예 컴포넌트가 언마운트 되도록 설정

--- src/app/components/common/header/overall-filter.tsx
+++ src/app/components/common/header/overall-filter.tsx
@@ -52,45 +52,45 @@ export default function OverallFilter() {
   }, [overallLimit]);
 
   return (
-    <div
-      className={classNames('relative mr-15 flex items-center justify-center border-r-1 border-r-slate-200/20 pr-15', {
-        hidden: !pathname.startsWith('/entry/'),
-      })}
-    >
-      <div className='flex cursor-pointer items-center justify-end gap-5 py-5 text-17' onClick={onSelectClick}>
-        <span className='font-semibold text-[#F98A58]'>
-          {overallLimit <= 55 ? '전체' : `${overallLimit} 이상`} 보기
-        </span>
-        {isOverallFilter ? <IoCaretUp className='text-white/50' /> : <IoCaretDown className='text-white/50' />}
+    pathname.startsWith('/entry/') && (
+      <div
+        className={classNames('relative mr-15 flex items-center justify-center border-r-1 border-r-slate-200/20 pr-15')}
+      >
+        <div className='flex cursor-pointer items-center justify-end gap-5 py-5 text-17' onClick={onSelectClick}>
+          <span className='font-semibold text-[#F98A58]'>
+            {overallLimit <= 55 ? '전체' : `${overallLimit} 이상`} 보기
+          </span>
+          {isOverallFilter ? <IoCaretUp className='text-white/50' /> : <IoCaretDown className='text-white/50' />}
+        </div>
+
+        {isOverallFilter && (
+          <ul className='absolute top-30 z-10 flex w-[200px] flex-col items-center justify-center rounded-lg bg-slate-800 p-15 shadow-lg'>
+            <div className='mb-15 flex flex-col gap-2 border-b-2 border-slate-200/20 pb-15 text-13 text-red-300 opacity-80'>
+              <span>오버롤 필터 변경 시,</span>
+              <span>선택한 선수들이 초기화됩니다.</span>
+            </div>
+
+            {buttonValueList.map((value) => (
+              <li key={value} className='mb-5 w-full flex-1 last:mb-0'>
+                <button
+                  className={classNames(
+                    'w-full border-1 p-10 transition-[color,_background-color,_opacity] duration-200',
+                    {
+                      'bg-slate-100 font-semibold text-[#e56d36]': value === overallLimit,
+                      'text-slate-400 opacity-50 hover:bg-slate-300 hover:text-slate-800 hover:opacity-90':
+                        value !== overallLimit,
+                    }
+                  )}
+                  value={value}
+                  onClick={onFilterClick}
+                >
+                  {value <= 55 ? '전체' : `${value} 이상`} 보기
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-
-      {isOverallFilter && (
-        <ul className='absolute top-30 z-10 flex w-[200px] flex-col items-center justify-center rounded-lg bg-slate-800 p-15 shadow-lg'>
-          <div className='mb-15 flex flex-col gap-2 border-b-2 border-slate-200/20 pb-15 text-13 text-red-300 opacity-80'>
-            <span>오버롤 필터 변경 시,</span>
-            <span>선택한 선수들이 초기화됩니다.</span>
-          </div>
-
-          {buttonValueList.map((value) => (
-            <li key={value} className='mb-5 w-full flex-1 last:mb-0'>
-              <button
-                className={classNames(
-                  'w-full border-1 p-10 transition-[color,_background-color,_opacity] duration-200',
-                  {
-                    'bg-slate-100 font-semibold text-[#e56d36]': value === overallLimit,
-                    'text-slate-400 opacity-50 hover:bg-slate-300 hover:text-slate-800 hover:opacity-90':
-                      value !== overallLimit,
-                  }
-                )}
-                value={value}
-                onClick={onFilterClick}
-              >
-                {value <= 55 ? '전체' : `${value} 이상`} 보기
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    )
   );
 }
